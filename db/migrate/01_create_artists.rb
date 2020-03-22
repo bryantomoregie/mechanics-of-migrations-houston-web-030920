bff0f758144716de4553b13830167ff444e5489c
bryantomoregie <bryantomoregie@yahoo.com>
2020-03-22 18:45:02 -0500

    Done.

---
+++ db/migrate/01_create_artists.rb
@@ -0,0 +1,19 @@
+class CreateArtists < ActiveRecord::Migration[5.2]
+    
+    def up
+    end
+   
+    def down
+    end
+    
+    def change 
+        create_table :artists, if_not_exist: true do |t|
+        t.string :name
+        t.string :genre
+        t.integer :age
+        t.string :hometown
+        end
+    end
+
+  end
+
